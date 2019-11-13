2ce63913a063df1953186160f1a3a862bca240f6
Wagner Leonardi <leonardiwagner@gmail.com>
2019-11-13 13:53:58 -0300

    fix publishing only on PR merge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Published on <strong>2019-11-13 13:54</strong>
         </p>
         <a
           className="App-link"
